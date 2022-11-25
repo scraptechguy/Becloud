118ff715ec5273ab4ac6a1bce20663049c3510e5
Rostislav Brož <scraptechguy@gmail.com>
2022-11-25 18:35:31 +0100

    Initial Commit

---
+++ Becloud/BecloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BecloudApp.swift
+//  Becloud
+//
+//  Created by Rostislav Brož on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BecloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
